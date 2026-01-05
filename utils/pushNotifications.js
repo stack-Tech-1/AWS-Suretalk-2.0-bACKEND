1fbf90bd72880ddeffaf04520a01b7d6d6a3ba34
stack-Tech-1 <stacktechnologies0@gmail.com>
2026-01-05 04:15:16 +0100

    database

--- utils/pushNotifications.js
+++ utils/pushNotifications.js
@@ -16,7 +16,7 @@ webpush.setVapidDetails(
 
 // Database connection
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DB_URL,
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
--- workers/messageScheduler.js
+++ workers/messageScheduler.js
@@ -8,7 +8,7 @@ const logger = require('../utils/logger');
 
 // Database connection
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DB_URL,
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
